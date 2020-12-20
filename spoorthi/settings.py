fff787b85b7209688303a81a3ae2d0d103ac96a9
shubh67678 <shubh67678@gmail.com>
2020-12-20 20:39:56 +0530

    timline and register list updates task (1), (6)

--- spoorthi/settings.py
+++ spoorthi/settings.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-EMAIL_USE_TLS=True
-# EMAIL_USE_SSL=True
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER=''
-EMAIL_HOST_PASSWORD=''
-EMAIL_PORT=587
 import os
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL=True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,12 +25,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'asd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','172.16.10.54','sports.spit.ac.in',]
+ALLOWED_HOSTS = ['127.0.0.1', '172.16.10.54', 'sports.spit.ac.in', ]
 
 
 # Application definition
@@ -125,4 +125,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),]
+    os.path.join(BASE_DIR, 'static'), ]
